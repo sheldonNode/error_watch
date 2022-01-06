b12ca6f7ebc3d4d12e189018316fd974dc0832a7
sheldonNode <96953213+sheldonNode@users.noreply.github.com>
2022-01-06 16:18:47 +0100

    Update watchlog.sh

--- watchlog.sh
+++ watchlog.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# sudo apt install inotify-tools
-
 DIR=/opt/xxnetwork/node-logs
-FILE=node-err.log
-ERROR=CUDA_ERROR_NO_DEVICE
+FILE=node.log
+GUAC_ERROR=CUD_ERROR_NO_DEVICE
+ERROR='ERROR\|FATAL'
 LOG_WATCH=/opt/xxnetwork/node_error_reboot.log
 
 inotifywait -m $DIR -e create -e modify |
@@ -12,12 +11,15 @@ while read dir ev file; do
  if [ "$file" = "$FILE" ]; then
   if [ "$ev" = "MODIFY" ]; then
    line=`tail -n1 $DIR/$file`
-   if echo $line | grep "$ERROR"; then
+   if echo $line | grep "$GUAC_ERROR"; then
      echo "$(date) restart server cause of  error: $ERROR in line: $line" >> $LOG_WATCH
-     su -c "reboot"
+     # be carefull that you server doesn´t comes into a restart loop 
+     su -c "shutdown -r +1"
    else
+     if  echo $line | grep -e "$ERROR"; then
      echo "$(date) error event: $line" >> $LOG_WATCH
+     fi
    fi
   fi
  fi
-done
+
